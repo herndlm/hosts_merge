0f8d19e5e8b4833e278929de85ef261a06af38e7
Martin Herndl <martin@herndl.org>
2015-08-25 09:20:53 +0200

    migrated to bash 1/2

--- misc/minimize_js_css.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $0)
-
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type css $BASEDIR/../public/css/styles.css -o $BASEDIR/../public/css/styles-min.css
--- public/hosts_windows.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-%~d0
-cd %~d0%~p0
-ATTRIB +A -H -R -S %windir%\system32\drivers\etc\HOSTS
-COPY /Y hosts.txt %windir%\system32\drivers\etc\HOSTS
-echo Hosts file has been updated. The ads should be gone now.
-Pause
-EXIT
