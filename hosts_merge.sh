c48369acb944dc59def2ad5ea09c0e717fb96797
Martin Herndl <martin@herndl.org>
2021-01-18 17:07:47 +0100

    feat: reformat code, remove unneeded files and dirs

--- hosts_merge.sh
+++ hosts_merge.sh
@@ -6,13 +6,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 readonly OS_NAME="$(uname -s)"
-if [[ $OS_NAME = "Darwin" ]]; then
+if [[ $OS_NAME == "Darwin" ]]; then
   readonly SED_COMMAND="gsed"
   readonly MD5_COMMAND="md5"
-elif [[ $OS_NAME = "Linux" ]]; then
+elif [[ $OS_NAME == "Linux" ]]; then
   readonly SED_COMMAND="sed"
   readonly MD5_COMMAND="md5sum"
 else
@@ -23,27 +23,98 @@ fi
 CURL_RETRY_NUM=5
 CURL_TIMEOUT=300
 
-file_blacklist=""
-file_whitelist=""
-file_result=""
-permissions_result=0
-FILE_LOG=""
-sources_hosts_format=()
-sources_domains_only=()
+readonly FILE_BLACKLIST="${DIR}/hosts_blacklist.txt"
+readonly FILE_WHITELIST="${DIR}/hosts_whitelist.txt"
+readonly FILE_RESULT="${DIR}/hosts.txt"
 
-# include user config
-# shellcheck source=config.sh
-source "${DIR}/config.sh"
+readonly PERMISSIONS_RESULT=644
 
-# create / define temp files
-file_temp=$(mktemp)
-file_temp_ipv6="${file_temp}.ipv6"
+readonly FILE_LOG="/tmp/hosts_merge.log"
+
+readonly SOURCES_HOST_FORMAT=(
+  # MVPS HOSTS
+  "https://winhelp2002.mvps.org/hosts.txt"
+  # CoinBlockerLists
+  "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/hosts"
+  "https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/hosts_browser"
+  # pgl@yoyo.org
+  "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0"
+  # AdAway
+  "https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt"
+  # FadeMind
+  "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
+  "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
+  "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts"
+  "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
+  "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
+  # StevenBlack
+  "https://raw.githubusercontent.com/StevenBlack/hosts/master/data/StevenBlack/hosts"
+  # Bad Boys Hosts
+  "https://raw.githubusercontent.com/mitchellkrogza/Badd-Boyz-Hosts/master/hosts"
+  # Dan Pollock hosts
+  "https://someonewhocares.org/hosts/zero/hosts"
+  # CAMELEON
+  "https://sysctl.org/cameleon/hosts"
+  # MalwareDomainList.com Hosts List
+  "https://www.malwaredomainlist.com/hostslist/hosts.txt"
+  # OCSP / CRL
+  "https://raw.githubusercontent.com/ScottHelme/revocation-endpoints/master/ocsp.txt"
+  "https://raw.githubusercontent.com/ScottHelme/revocation-endpoints/master/crl.txt"
+  # Windows 10 Anti Spy
+  "https://www.encrypt-the-planet.com/downloads/hosts"
+  # Blacklist
+  "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
+  # Ultimate Hosts Blacklist
+  "https://hosts.ubuntu101.co.za/hosts"
+  # DataMaster-Android-AdBlock-Hosts
+  "https://raw.githubusercontent.com/DataMaster-2501/DataMaster-Android-AdBlock-Hosts/master/hosts"
+  # NoCoin Filter List
+  "https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt"
+  # WindowsSpyBlocker - Hosts spy rules
+  "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+  # First-party trackers host list
+  "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
+  # abuse.ch URLhaus Host file
+  "https://urlhaus.abuse.ch/downloads/hostfile/"
+  # DigitalSite Threat-Intel
+  "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
+  # Phishing Army
+  "https://phishing.army/download/phishing_army_blocklist_extended.txt"
+  # GoodByeAds
+  "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Hosts/GoodbyeAds.txt"
+  "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Samsung-AdBlock.txt"
+  "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Spotify-AdBlock.txt"
+  "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-Xiaomi-Extension.txt"
+  "https://raw.githubusercontent.com/jerryn70/GoodbyeAds/master/Extension/GoodbyeAds-YouTube-AdBlock.txt"
+  # NoTracking
+  "https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt"
+)
+
+readonly SOURCES_DOMAINS_ONLY=(
+  # malwaredomains
+  "https://malwaredomains.usu.edu/immortal_domains.txt"
+  "https://malwaredomains.usu.edu/justdomains"
+  # disconnect.me
+  "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
+  "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
+  "https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt"
+  "https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt"
+  # Perflyst's Smart-TV Blocklist for Pi-hole
+  "https://raw.githubusercontent.com/Perflyst/PiHoleBlocklist/master/SmartTV.txt"
+  # Spam404
+  "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt"
+  # Personal Blocklist by WaLLy3K
+  "https://v.firebog.net/hosts/static/w3kbl.txt"
+)
+
+readonly FILE_TEMP=$(mktemp)
+readonly FILE_TEMP_IPV6="${FILE_TEMP}.ipv6"
 
 # cleanup if user presses CTRL-C
 cleanup() {
   log "cleaning up temp files"
-  rm "${file_temp}" >/dev/null 2>&1
-  rm "${file_temp_ipv6}" >/dev/null 2>&1
+  rm "${FILE_TEMP}" >/dev/null 2>&1
+  rm "${FILE_TEMP_IPV6}" >/dev/null 2>&1
   exit 0
 }
 trap cleanup INT SIGHUP SIGINT SIGTERM
@@ -54,7 +125,7 @@ log() {
   message="$(date) $1"
   # log to file if variable is not empty
   if [ -n "$FILE_LOG" ]; then
-    echo -e "${message}" >> "$FILE_LOG"
+    echo -e "${message}" >>"$FILE_LOG"
   fi
   # print argument as message if verbose is set
   if [ "${mode_verbose}" -eq 1 ]; then
@@ -120,7 +191,7 @@ for var in "$@"; do
   elif [ "${var}" = "clean" ]; then
     mode_clean=1
   elif [[ "${var}" == "output="* ]]; then
-    file_result=${var/output=/}
+    FILE_RESULT=${var/output=/}
   elif [[ "${var}" == "ipv6dup" ]]; then
     ipv6dup=1
   # unknown or wrong command, print usage
@@ -131,11 +202,11 @@ for var in "$@"; do
 done
 
 # read blacklist and whitelist data
-log "read blacklist and whitelist data from '$file_blacklist' and '$file_whitelist'"
+log "read blacklist and whitelist data from '$FILE_BLACKLIST' and '$FILE_WHITELIST'"
 data_blacklist=()
-readarray data_blacklist <"${file_blacklist}"
+readarray data_blacklist <"${FILE_BLACKLIST}"
 data_whitelist=()
-readarray data_whitelist <"${file_whitelist}"
+readarray data_whitelist <"${FILE_WHITELIST}"
 # clean blacklist and whitelist data
 log "clean blacklist and whitelist data"
 for index in "${!data_blacklist[@]}"; do
@@ -146,42 +217,45 @@ for index in "${!data_whitelist[@]}"; do
 done
 
 # download all sources in hosts format
-for source_hosts_format in "${sources_hosts_format[@]}"; do
-  log "downloading hosts source '$source_hosts_format' to '$file_temp'"
+for source_hosts_format in "${SOURCES_HOST_FORMAT[@]}"; do
+  log "downloading hosts source '$source_hosts_format' to '$FILE_TEMP'"
   curl --location -sS --connect-timeout ${CURL_TIMEOUT} --max-time ${CURL_TIMEOUT} \
-    --retry ${CURL_RETRY_NUM} "${source_hosts_format}" >> "${file_temp}"
+      --retry ${CURL_RETRY_NUM} "${source_hosts_format}" >> "${FILE_TEMP}"
 done
 
 # download all domain only sources (we're cleaning the lines and prepending ip adresses)
-for source_domains_only in "${sources_domains_only[@]}"; do
-  log "downloading domain only source '${source_domains_only}' to '${file_temp}'"
+for source_domains_only in "${SOURCES_DOMAINS_ONLY[@]}"; do
+  log "downloading domain only source '${source_domains_only}' to '${FILE_TEMP}'"
   curl --location -sS --connect-timeout ${CURL_TIMEOUT} --max-time ${CURL_TIMEOUT} \
-    --retry ${CURL_RETRY_NUM} "${source_domains_only}" | grep -v '^#' | "${SED_COMMAND}" -e 's/^/0.0.0.0 /' >> "${file_temp}"
+      --retry ${CURL_RETRY_NUM} "${source_domains_only}" \
+    | grep -v '^#' \
+    | "${SED_COMMAND}" -e 's/^/0.0.0.0 /' >>"${FILE_TEMP}"
+
 done
 
-log "cleaning up '${file_temp}'"
+log "cleaning up '${FILE_TEMP}'"
 # Remove MS-DOS carriage returns
-"${SED_COMMAND}" -i -e 's/\r//g' "${file_temp}"
+"${SED_COMMAND}" -i -e 's/\r//g' "${FILE_TEMP}"
 # Replace 127.0.0.1 with 0.0.0.0 because then we don't have to wait for the resolver to fail
-"${SED_COMMAND}" -i -e 's/127.0.0.1/0.0.0.0/g' "${file_temp}"
+"${SED_COMMAND}" -i -e 's/127.0.0.1/0.0.0.0/g' "${FILE_TEMP}"
 # Remove all comments
-"${SED_COMMAND}" -i -e 's/#.*//g' "${file_temp}"
+"${SED_COMMAND}" -i -e 's/#.*//g' "${FILE_TEMP}"
 # Strip trailing spaces and tabs
-"${SED_COMMAND}" -i -e 's/[ \t]*$//g' "${file_temp}"
+"${SED_COMMAND}" -i -e 's/[ \t]*$//g' "${FILE_TEMP}"
 # Replace tabs with a space
-"${SED_COMMAND}" -i -e 's/\t/ /g' "${file_temp}"
+"${SED_COMMAND}" -i -e 's/\t/ /g' "${FILE_TEMP}"
 # Remove lines containing invalid characters
-"${SED_COMMAND}" -i -e '/[^a-zA-Z0-9\t\. _-]/d' "${file_temp}"
+"${SED_COMMAND}" -i -e '/[^a-zA-Z0-9\t\. _-]/d' "${FILE_TEMP}"
 # Replace multiple spaces with one space
-"${SED_COMMAND}" -i -e 's/ \{2,\}/ /g' "${file_temp}"
+"${SED_COMMAND}" -i -e 's/ \{2,\}/ /g' "${FILE_TEMP}"
 # Remove lines that do not start with "0.0.0.0"
-"${SED_COMMAND}" -i -e '/^0.0.0.0/!d' "${file_temp}"
+"${SED_COMMAND}" -i -e '/^0.0.0.0/!d' "${FILE_TEMP}"
 # Remove localhost lines
-"${SED_COMMAND}" -i -r -e '/^0\.0\.0\.0 local(host)*(.localdomain)*$/d' "${file_temp}"
+"${SED_COMMAND}" -i -r -e '/^0\.0\.0\.0 local(host)*(.localdomain)*$/d' "${FILE_TEMP}"
 # Remove lines that start correct but have no or empty domain
-"${SED_COMMAND}" -i -e '/^0\.0\.0\.0 \{0,\}$/d' "${file_temp}"
+"${SED_COMMAND}" -i -e '/^0\.0\.0\.0 \{0,\}$/d' "${FILE_TEMP}"
 # Remove lines with invalid domains (domains must start with an alphanumeric character)
-"${SED_COMMAND}" -i -e '/^0\.0\.0\.0 [^a-zA-Z0-9]/d' "${file_temp}"
+"${SED_COMMAND}" -i -e '/^0\.0\.0\.0 [^a-zA-Z0-9]/d' "${FILE_TEMP}"
 
 # check mode (checks entries of the white- & blacklist)
 # clean mode (remove whitelist entries that do not exist in the hosts file and remove blacklist
@@ -196,19 +270,19 @@ if [ ${mode_check} -eq 1 ] || [ ${mode_clean} -eq 1 ]; then
       continue
     fi
 
-    if grep -q "^0.0.0.0 ${data_whitelist_entry}$" "${file_temp}"; then
+    if grep -q "^0.0.0.0 ${data_whitelist_entry}$" "${FILE_TEMP}"; then
       # entry that is not blocked any more
       if [ ${mode_clean} -eq 1 ]; then
         echo "${counter}/${count} removing entry '${data_whitelist_entry}' from the whitelist"
-        "${SED_COMMAND}" -i -e "/${data_whitelist_entry}/d" "${file_whitelist}"
+        "${SED_COMMAND}" -i -e "/${data_whitelist_entry}/d" "${FILE_WHITELIST}"
       else
-        echo "${counter}/${count} whitelist entry '${data_whitelist_entry}' is not existing in '${file_temp}'"
+        echo "${counter}/${count} whitelist entry '${data_whitelist_entry}' is not existing in '${FILE_TEMP}'"
       fi
     elif ! domain_resolves "${data_whitelist_entry}"; then
       # entry that is not resolving any more
       if [ ${mode_clean} -eq 1 ]; then
         echo "${counter}/${count} removing non-resolving entry '${data_whitelist_entry}' from the whitelist"
-        "${SED_COMMAND}" -i -e "/${data_whitelist_entry}/d" "${file_whitelist}"
+        "${SED_COMMAND}" -i -e "/${data_whitelist_entry}/d" "${FILE_WHITELIST}"
       else
         echo "${counter}/${count} whitelist entry '${data_whitelist_entry}' is not resolving"
       fi
@@ -223,19 +297,19 @@ if [ ${mode_check} -eq 1 ] || [ ${mode_clean} -eq 1 ]; then
       continue
     fi
 
-    if grep -q "^0.0.0.0 ${data_blacklist_entry}$" "${file_temp}"; then
+    if grep -q "^0.0.0.0 ${data_blacklist_entry}$" "${FILE_TEMP}"; then
       # entry that is already blocked
       if [ ${mode_clean} -eq 1 ]; then
         echo "${counter}/${count} removing entry '${data_blacklist_entry}' from the blacklist"
-        "${SED_COMMAND}" -i -e "/${data_blacklist_entry}/d" "${file_blacklist}"
+        "${SED_COMMAND}" -i -e "/${data_blacklist_entry}/d" "${FILE_BLACKLIST}"
       else
-        echo "${counter}/${count} blacklist entry '${data_blacklist_entry}' is existing in '${file_temp}'"
+        echo "${counter}/${count} blacklist entry '${data_blacklist_entry}' is existing in '${FILE_TEMP}'"
       fi
     elif ! domain_resolves "${data_blacklist_entry}"; then
       # entry that is not resolving any more
       if [ ${mode_clean} -eq 1 ]; then
         echo "${counter}/${count} removing non-resolving entry '${data_blacklist_entry}' from the blacklist"
-        "${SED_COMMAND}" -i -e "/${data_blacklist_entry}/d" "${file_blacklist}"
+        "${SED_COMMAND}" -i -e "/${data_blacklist_entry}/d" "${FILE_BLACKLIST}"
       else
         echo "${counter}/${count} blacklist entry '${data_blacklist_entry}' is not resolving"
       fi
@@ -247,7 +321,7 @@ fi
 log "removing all whitelisted entries"
 for data_whitelist_entry in "${data_whitelist[@]}"; do
   if [ -n "${data_whitelist_entry}" ]; then
-    "${SED_COMMAND}" -i -e "/ ${data_whitelist_entry}/d" "${file_temp}"
+    "${SED_COMMAND}" -i -e "/ ${data_whitelist_entry}/d" "${FILE_TEMP}"
   fi
 done
 
@@ -255,21 +329,21 @@ done
 log "adding all blacklisted entries"
 for data_blacklist_entry in "${data_blacklist[@]}"; do
   if [ -n "${data_blacklist_entry}" ]; then
-    echo "0.0.0.0 ${data_blacklist_entry}" >> "${file_temp}"
+    echo "0.0.0.0 ${data_blacklist_entry}" >>"${FILE_TEMP}"
   fi
 done
 
 # sort file entries and remove multiple occuring entries
 log "sort file entries and remove multiple occuring entries"
-sort -u -o "${file_temp}" "${file_temp}"
+sort -u -o "${FILE_TEMP}" "${FILE_TEMP}"
 
 # duplicate data for IPv6 (e.g. dnsmasq needs such entries to block IPv6 hosts)
 if [ ${ipv6dup} -eq 1 ]; then
-  log "duplicate data for IPv6 in temp file '${file_temp_ipv6}'"
-  cp "${file_temp}" "${file_temp_ipv6}"
-  "${SED_COMMAND}" -i -e 's/0.0.0.0/::0/g' "${file_temp_ipv6}"
-  cat "${file_temp_ipv6}" >> "${file_temp}"
-  rm -f "${file_temp_ipv6}"
+  log "duplicate data for IPv6 in temp file '${FILE_TEMP_IPV6}'"
+  cp "${FILE_TEMP}" "${FILE_TEMP_IPV6}"
+  "${SED_COMMAND}" -i -e 's/0.0.0.0/::0/g' "${FILE_TEMP_IPV6}"
+  cat "${FILE_TEMP_IPV6}" >>"${FILE_TEMP}"
+  rm -f "${FILE_TEMP_IPV6}"
 fi
 
 # generate and write file header
@@ -279,17 +353,17 @@ data_header="# clean merged adblocking-hosts file\n\
 \n\
 127.0.0.1 localhost\n\
 ::1 localhost\n"
-"${SED_COMMAND}" -i "1i${data_header}" "${file_temp}"
+"${SED_COMMAND}" -i "1i${data_header}" "${FILE_TEMP}"
 
 # rotate in place and fix permissions if md5sum old - new is different, otherwise we're done
-if [ ! -f "${file_result}" ] || [ "$(md5file "${file_result}")" != "$(md5file "${file_temp}")" ]; then
+if [ ! -f "${FILE_RESULT}" ] || [ "$(md5file "${FILE_RESULT}")" != "$(md5file "${FILE_TEMP}")" ]; then
   # rotate in place
-  log "move temp file '$file_temp' to '$file_result'"
-  mv -f "${file_temp}" "${file_result}"
+  log "move temp file '$FILE_TEMP' to '$FILE_RESULT'"
+  mv -f "${FILE_TEMP}" "${FILE_RESULT}"
 
   # fixup permissions (we don't want that limited temp perms)
-  log "chmod '${file_result}' to '${permissions_result}'"
-  chmod ${permissions_result} "${file_result}"
+  log "chmod '${FILE_RESULT}' to '${PERMISSIONS_RESULT}'"
+  chmod ${PERMISSIONS_RESULT} "${FILE_RESULT}"
 else
   log "no changes, skip rotating in place"
 fi
